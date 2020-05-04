b5b159d201bb574b1926627e84c27c520d73f622
pixfid <pixfid@gmail.com>
2020-05-04 17:57:14 +0300

    Rework AdHosts.go
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/AdHosts.go
+++ kaspersky/AdHosts.go
@@ -23,17 +23,133 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+//	AdHosts Class Reference
+//
+//	Scanned active directory OU structure.
+//
+//	Allows to enumerate scanned active directory OU structure.
+//
+//	List of all members:
 type AdHosts struct {
 	client *Client
+}
+
+//FindAdGroupsParams struct
+type FindAdGroupsParams struct {
+	VecFieldsToReturn []string           `json:"vecFieldsToReturn,omitempty"`
+	VecFieldsToOrder  []VecFieldsToOrder `json:"vecFieldsToOrder,empty"`
+	POptions          POptions           `json:"pOptions,omitempty"`
+	LMaxLifeTime      int64              `json:"lMaxLifeTime,omitempty"`
+}
+
+type POptions struct {
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value"`
+}
+
+//	ADHostIterator struct
+type ADHostIterator struct {
+	WstrIterator string `json:"wstrIterator"`
+	PxgRetVal    int64  `json:"PxgRetVal"`
+}
+
+//Enumerates AD groups.
+//
+//	- Parameters:
+//	- Example:
+//		VecFieldsToReturn: []string{
+//			"adhst_adgroup_id",
+//			"adhst_adgroup_distinguished_name",
+//			"adhst_adgroup_name",
+//			"adhst_adgroup_sam_name",
+//		},
+//		VecFieldsToOrder: []kaspersky.VecFieldsToOrder{
+//			{
+//				Type: "params",
+//				Value: kaspersky.Value{
+//					Asc:  true,
+//					Name: "adhst_adgroup_id",
+//				},
+//			},
+//		},
+//		POptions: kaspersky.POptions{
+//			Type:  "params",
+//			Value: "adhst_id",
+//		},
+//		LMaxLifeTime: 100,
+//	}
+//	- Where:
+//	- vecFieldsToReturn	(array) attributes to return, possible values
+//	"adhst_adgroup_id" (paramInt) AD group id
+//	"adhst_adgroup_distinguished_name" (paramString) AD group unique name (distinguished name)
+//	"adhst_adgroup_name" (paramString) AD group short name (may be non-unique)
+//	"adhst_adgroup_sam_name" (paramString) AD group NT4-compatible name (unique but may be absent)
+//
+//	- vecFieldsToOrder	(array) array of containers each of them containing two attributes :
+//	"Name" (paramString) name of attributes used for sorting
+//	"Asc" (paramBool) ascending if true descending otherwise
+//
+//	- pOptions	(params) options, possible attributes:
+//	"adhst_id" - return groups for given ad host
+//
+//	- lMaxLifeTime	(int) max result-set lifetime in seconds, not more than 7200
+//	- [out]	wstrIterator(string) result-set ID, identifier of the server-side ordered collection.
+//The result-set is destroyed and associated memory is freed in following cases:
+//
+//Passed lMaxLifeTime seconds after last access to the result-set (by methods ChunkAccessor.GetItemsCount and ChunkAccessor.GetItemsChunk
+//Session to the Administration Server has been closed.
+//ChunkAccessor.Release has been called.
+func (ah *AdHosts) FindAdGroups(ctx context.Context, params FindAdGroupsParams) (*ADHostIterator, []byte, error) {
+	postData, _ := json.Marshal(params)
+
+	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.FindAdGroups", bytes.NewBuffer(postData))
+
+	aDHostIterator := new(ADHostIterator)
+
+	raw, err := ah.client.Do(ctx, request, &aDHostIterator)
+	return aDHostIterator, raw, err
 }
 
 type ChildComputersParams struct {
 	IDOU              int64    `json:"idOU"`
 	VecFieldsToReturn []string `json:"vecFieldsToReturn"`
 	LMaxLifeTime      int64    `json:"lMaxLifeTime"`
+}
+
+//ChildComputerParams struct
+type ChildComputerParams struct {
+	IDAdhst           int64    `json:"idAdhst,omitempty"`
+	VecFieldsToReturn []string `json:"vecFieldsToReturn,omitempty"`
+}
+
+//AdHstIDParent struct contain AD host attributes.
+type AdHstIDParent struct {
+	PxgRetVal AdHstIDParentPxgRetVal `json:"PxgRetVal"`
+}
+
+type AdHstIDParentPxgRetVal struct {
+	AdhstIDParent int64 `json:"adhst_idParent"`
+}
+
+//	Retrieves AD host attributes.
+//
+//	Parameters:
+//	- idAdhst	(int) host identifier (same as GetChildComputers attribute adhst_id)
+//	- vecFieldsToReturn	(array) Array of propery names.
+//	See List of host attributes and List of attributes of organization units for attribute names.
+func (ah *AdHosts) GetChildComputer(ctx context.Context, params ChildComputerParams) (*AdHstIDParent,
+	[]byte, error) {
+	postData, _ := json.Marshal(params)
+
+	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.GetChildComputer", bytes.NewBuffer(postData))
+
+	adHstIdParent := new(AdHstIDParent)
+	raw, err := ah.client.Do(ctx, request, &adHstIdParent)
+	return adHstIdParent, raw, err
 }
 
 //Acquire list of hosts located in "Unassigned computers" for specified OU.
@@ -89,9 +205,9 @@ type ChildComputersParams struct {
 //	Passed lMaxLifeTime seconds after last access to the result-set (by methods ChunkAccessor.GetItemsCount and ChunkAccessor.GetItemsChunk
 //	Session to the Administration Server has been closed.
 //	ChunkAccessor.Release has been called.
-func (ah *AdHosts) GetChildComputers(ctx context.Context, ccParams ChildComputersParams) (*PxgValStr, []byte, error) {
+func (ah *AdHosts) GetChildComputers(ctx context.Context, params ChildComputersParams) (*PxgValStr, []byte, error) {
 
-	postData, _ := json.Marshal(ccParams)
+	postData, _ := json.Marshal(params)
 
 	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.GetChildComputers", bytes.NewBuffer(postData))
 
@@ -102,6 +218,7 @@ func (ah *AdHosts) GetChildComputers(ctx context.Context, ccParams ChildComputer
 	return pxgValStr, raw, err
 }
 
+//ChildOUParams struct
 type ChildOUParams struct {
 	IDOU         int64    `json:"idOU,omitempty"`
 	PFields      []string `json:"pFields,omitempty"`
@@ -133,9 +250,9 @@ type ChildOUParams struct {
 //Passed lMaxLifeTime seconds after last access to the result-set (by methods ChunkAccessor.GetItemsCount and ChunkAccessor.GetItemsChunk
 //Session to the Administration Server has been closed.
 //ChunkAccessor.Release has been called.
-func (ah *AdHosts) GetChildOUs(ctx context.Context, cup ChildOUParams) (*PxgValStr,
+func (ah *AdHosts) GetChildOUs(ctx context.Context, params ChildOUParams) (*PxgValStr,
 	[]byte, error) {
-	postData, _ := json.Marshal(cup)
+	postData, _ := json.Marshal(params)
 	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.GetChildOUs", bytes.NewBuffer(postData))
 	pxgValStr := new(PxgValStr)
 	raw, err := ah.client.Do(ctx, request, &pxgValStr)
@@ -147,6 +264,7 @@ type OUAttributesParams struct {
 	PFields []string `json:"pFields"`
 }
 
+//OUAttributes struct
 type OUAttributes struct {
 	Attributes Attributes `json:"PxgRetVal"`
 }
@@ -166,7 +284,6 @@ type AdhstBinOu struct {
 	Value string `json:"value"`
 }
 
-//TODO Acquire attributes of specified OU.
 //Returns attributes of specified OU
 //
 //	- Parameters: OUAttributesParams
@@ -192,8 +309,8 @@ type AdhstBinOu struct {
 //	}
 //	- Returns:
 //   (params) object containing specified attributes
-func (ah *AdHosts) GetOU(ctx context.Context, oua OUAttributesParams) (*OUAttributes, []byte, error) {
-	postData, _ := json.Marshal(oua)
+func (ah *AdHosts) GetOU(ctx context.Context, params OUAttributesParams) (*OUAttributes, []byte, error) {
+	postData, _ := json.Marshal(params)
 
 	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.GetOU", bytes.NewBuffer(postData))
 	oUAttributes := new(OUAttributes)
@@ -201,133 +318,29 @@ func (ah *AdHosts) GetOU(ctx context.Context, oua OUAttributesParams) (*OUAttrib
 	return oUAttributes, raw, err
 }
 
-// TODO Updates OU properties.
-func (ah *AdHosts) UpdateOU(idOU int, pData interface{}) {
-
-}
-
-//FindAdGroupsParams struct
-type FindAdGroupsParams struct {
-	VecFieldsToReturn []string           `json:"vecFieldsToReturn,omitempty"`
-	VecFieldsToOrder  []VecFieldsToOrder `json:"vecFieldsToOrder,empty"`
-	POptions          POptions           `json:"pOptions,omitempty"`
-	LMaxLifeTime      int64              `json:"lMaxLifeTime,omitempty"`
-}
-
-type POptions struct {
-	Type  string `json:"type,omitempty"`
-	Value string `json:"value"`
-}
-
-//ADHostIteratorArray struct
-type ADHostIteratorArray struct {
-	PChunk    PChunk `json:"pChunk"`
-	PxgRetVal int64  `json:"PxgRetVal"`
-}
-
-type PChunk struct {
-	KlcspIteratorArray []KlcspIteratorArray `json:"KLCSP_ITERATOR_ARRAY"`
-}
-
-type KlcspIteratorArray struct {
-	Type         string       `json:"type"`
-	ADHostObject ADHostObject `json:"value"`
-}
-
-type ADHostObject struct {
-	AdhstAdgroupDistinguishedName string `json:"adhst_adgroup_distinguished_name"`
-	AdhstAdgroupID                int64  `json:"adhst_adgroup_id"`
-	AdhstAdgroupName              string `json:"adhst_adgroup_name"`
-	AdhstAdgroupSamName           string `json:"adhst_adgroup_sam_name"`
-}
-
-//Enumerates AD groups.
+//	Updates OU properties.
 //
-//	- Parameters:
-//	- Example:
-//		VecFieldsToReturn: []string{
-//			"adhst_adgroup_id",
-//			"adhst_adgroup_distinguished_name",
-//			"adhst_adgroup_name",
-//			"adhst_adgroup_sam_name",
-//		},
-//		VecFieldsToOrder: []kaspersky.VecFieldsToOrder{
-//			{
-//				Type: "params",
-//				Value: kaspersky.Value{
-//					Asc:  true,
-//					Name: "adhst_adgroup_id",
-//				},
-//			},
-//		},
-//		POptions: kaspersky.POptions{
-//			Type:  "params",
-//			Value: "adhst_id",
-//		},
-//		LMaxLifeTime: 100,
-//	}
-//	- Where:
-//	- vecFieldsToReturn	(array) attributes to return, possible values
-//	"adhst_adgroup_id" (paramInt) AD group id
-//	"adhst_adgroup_distinguished_name" (paramString) AD group unique name (distinguished name)
-//	"adhst_adgroup_name" (paramString) AD group short name (may be non-unique)
-//	"adhst_adgroup_sam_name" (paramString) AD group NT4-compatible name (unique but may be absent)
+//	Parameters:
+//	- idOU	(int) id of organization unit
+//	- pData	(params) may contain following values:
 //
-//	- vecFieldsToOrder	(array) array of containers each of them containing two attributes :
-//	"Name" (paramString) name of attributes used for sorting
-//	"Asc" (paramBool) ascending if true descending otherwise
-//
-//	- pOptions	(params) options, possible attributes:
-//	"adhst_id" - return groups for given ad host
-//
-//	- lMaxLifeTime	(int) max result-set lifetime in seconds, not more than 7200
-//	- [out]	wstrIterator(string) result-set ID, identifier of the server-side ordered collection.
-//The result-set is destroyed and associated memory is freed in following cases:
-//
-//Passed lMaxLifeTime seconds after last access to the result-set (by methods ChunkAccessor.GetItemsCount and ChunkAccessor.GetItemsChunk
-//Session to the Administration Server has been closed.
-//ChunkAccessor.Release has been called.
-func (ah *AdHosts) FindAdGroups(ctx context.Context, fagP FindAdGroupsParams) (*ADHostIterator, []byte, error) {
-	postData, _ := json.Marshal(fagP)
+//	"adhst_enable_ad_scan" (see Active Directory-specific attributes for organization units and computers.)
+func (ah *AdHosts) UpdateOU(ctx context.Context, idOU int, params interface{}) ([]byte, error) {
+	//postData, _ := json.Marshal(params)
+	//TODO Find correct request
+	postData := []byte(fmt.Sprintf(`
+	{
+		"idOU":   %d,
+		"pData": {
+			"type" : "params",
+			"value" : {
+				"adhst_enable_ad_scan" : 1 
+			}
+		}
+	}`, idOU))
 
-	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.FindAdGroups", bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.UpdateOU", bytes.NewBuffer(postData))
+	raw, err := ah.client.Do(ctx, request, nil)
 
-	aDHostIterator := new(ADHostIterator)
-
-	raw, err := ah.client.Do(ctx, request, &aDHostIterator)
-	return aDHostIterator, raw, err
-}
-
-//ChildComputerParams struct
-type ChildComputerParams struct {
-	IDAdhst           int64    `json:"idAdhst,omitempty"`
-	VecFieldsToReturn []string `json:"vecFieldsToReturn,omitempty"`
-}
-
-//AdHstIDParent struct contain AD host attributes.
-type AdHstIDParent struct {
-	PxgRetVal AdHstIDParentPxgRetVal `json:"PxgRetVal"`
-}
-
-type AdHstIDParentPxgRetVal struct {
-	AdhstIDParent int64 `json:"adhst_idParent"`
-}
-
-//TODO Retrieves AD host attributes.
-//
-//Parameters:
-//	- idAdhst	(int64) host identifier (same as GetChildComputers attribute adhst_id)
-//	- vecFieldsToReturn	(array) Array of propery names.
-//	See List of host attributes and List of attributes of organization units for attribute names.
-//Returns:
-//	- (params) required data
-func (ah *AdHosts) GetChildComputer(ctx context.Context, ccp ChildComputerParams) (*AdHstIDParent,
-	[]byte, error) {
-	postData, _ := json.Marshal(ccp)
-
-	request, err := http.NewRequest("POST", ah.client.Server+"/api/v1.0/AdHosts.GetChildComputer", bytes.NewBuffer(postData))
-
-	adHstIdParent := new(AdHstIDParent)
-	raw, err := ah.client.Do(ctx, request, &adHstIdParent)
-	return adHstIdParent, raw, err
+	return raw, err
 }
