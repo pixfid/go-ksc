e46f09899fb1fe7ac2cb9ed7985e2f2ee5139ff1
pixfid <pixfid@gmail.com>
2020-05-04 14:37:55 +0300

    Rework some methods in LicenseKeys.go:
    Remove unused structs
    
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/LicenseKeys.go
+++ kaspersky/LicenseKeys.go
@@ -28,8 +28,57 @@ import (
 	"net/http"
 )
 
+//	LicenseKeys Class Reference
+//
+//	Operating with keys
+//
+//	Public Member Functions:
 type LicenseKeys struct {
 	client *Client
+}
+
+//	Install a key on the server.
+//
+//	Parameters:
+//	- pKeyInfo	(params) input data container, mandatory:
+//
+//Installing a key by specifying contents of a key file:
+//
+//	 ● "KLLICSRV_KEYDATA" - key data container, mandatory (paramParams).
+//	-	◯ "KLLIC_IFKEYFILE" - set to true in this case, mandatory (paramBool)
+//	-	◯ "KLLIC_LICFILE" - keyfile body, mandatory (paramBinary)
+//
+//	Installing a key by specifying just an activation 2.0 code:
+//
+//	 ● "KLLICSRV_KEYDATA" - key data container, mandatory (paramParams).
+//	-	◯ "KLLIC_IFKEYFILE" - set to false in this case (paramBool)
+//	-	◯ "KLLIC_LICFILE" - ASCII-encoded string with activation code in format of XXXXX-XXXXX-XXXXX-XXXXX (
+//	exactly 23 characters long buffer) (paramBinary)
+//
+//	Returns:
+//
+//	(string) serial number of the installed license.
+//
+//	Exceptions:
+//
+//	Throws	exception in case of error.
+func (lk *LicenseKeys) InstallKey(ctx context.Context, pKeyInfo interface{}) bool {
+
+	postData, _ := json.Marshal(pKeyInfo)
+
+	request, err := http.NewRequest("POST", lk.client.Server+"/api/v1.0/LicenseKeys.InstallKey",
+		bytes.NewBuffer(postData))
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := lk.client.Do(ctx, request, nil)
+
+	if raw != nil {
+		return true
+	}
+	return false
 }
 
 //TODO Download license key files from activation key servers V1.
@@ -53,12 +102,64 @@ func (lk *LicenseKeys) DownloadKeyFiles(ctx context.Context, wstrActivationCode 
 		log.Fatal(err.Error())
 	}
 
-	jsonData, err := lk.client.Do(ctx, request, nil)
+	raw, err := lk.client.Do(ctx, request, nil)
 
-	if jsonData != nil {
+	if raw != nil {
 		return true
 	}
 	return false
+}
+
+//AcquireKeyHostsParams struct
+type AcquireKeyHostsParams struct {
+	PInData        PInData  `json:"pInData"`
+	PFields        []string `json:"pFields"`
+	PFieldsToOrder []string `json:"pFieldsToOrder"`
+	LTimeoutSEC    int64    `json:"lTimeoutSec"`
+}
+
+type PInData struct {
+	KllicSerial string `json:"KLLIC_SERIAL"`
+}
+
+type HostsKeyIterator struct {
+	LKeyCount    int64  `json:"lKeyCount"`
+	WstrIterator string `json:"wstrIterator"`
+}
+
+//	Get an array of hosts that are currently using the specified key.
+//
+//	Parameters:
+//	- pInData	(params) container of input attributes:
+//	- "KLLIC_SERIAL" - serial number of the key which the hosts array will be returned for, mandatory (paramString).
+//	- pFields	(array) of paramString, array of host attribute names to return.
+//	See List of host-specific license attributes for attribute names.
+//	- pFieldsToOrder	(array) of paramString, array of host attributes to be used for ordering
+//	- pOptions	(params) currently ignored.
+//	- lTimeoutSec	(int) iterator timeout in seconds. Output iterator will be available for this time long.
+//		Default is zero which means 15 minutes long.
+//
+//	Return:
+//	- lKeyCount	(int) count of keys returned via enumerator.
+//	- wstrIterator	(wstring) forward-iterator name for accesing key attributes through GroupSyncIterator.
+//
+//	Note:
+//	Call GroupSyncIterator::GetNextItems to iterate. Call GroupSyncIterator::ReleaseIterator when you are done.
+func (lk *LicenseKeys) AcquireKeyHosts(ctx context.Context, params AcquireKeyHostsParams) (*HostsKeyIterator,
+	[]byte, error) {
+	postData, _ := json.Marshal(params)
+
+	request, err := http.NewRequest("POST", lk.client.Server+"/api/v1.0/LicenseKeys.AcquireKeyHosts",
+		bytes.NewBuffer(postData))
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	hostsKeyIterator := new(HostsKeyIterator)
+	raw, err := lk.client.Do(ctx, request, &hostsKeyIterator)
+
+	return hostsKeyIterator, raw, nil
 }
 
 //	EnumKeysParams struct
@@ -67,54 +168,34 @@ type EnumKeysParams struct {
 	LTimeoutSEC int64    `json:"lTimeoutSec,omitempty"`
 }
 
-//	LicenseKeysIterator struct
-type LicenseKeysIterator struct {
-	LKeyCount    int64  `json:"lKeyCount,omitempty"`
-	WstrIterator string `json:"wstrIterator,omitempty"`
-}
+// Enumerate keys.
+//
+// Parameters:
+//	- pFields	(array) of paramString, array of attribute names to return,
+//	mandatory . See List of license key attributes for attribute names.
+//	- pFieldsToOrder	(array) of paramString, array of attributes to be used for ordering, mandatory
+//	- pOptions	(params) enumeration options, optional :
+//	"KLLICSRV_ENOPT_INSTALLED_ONLY" - true if you are enumerating only installed keys.
+//	Default is false - method will return all keys including ones not-installed on adm. server (product reported keys).
+//	- lTimeoutSec	(int) iterator timeout in seconds, optional.
+//
+//	Return: Output iterator will be available for this time long. Default is zero which means 15 minutes long.
+//	- lKeyCount	(int) count of keys returned via enumerator.
+//	- wstrIterator	(wstring) forward-iterator name for accesing key attributes through GroupSyncIterator.
+//
+//	Call GroupSyncIterator::GetNextItems to iterate. Call GroupSyncIterator::ReleaseIterator when you are done.
+//
+//Exceptions:
+//	Throws	exception in case of error.
+func (lk *LicenseKeys) EnumKeys(ctx context.Context, params EnumKeysParams, v interface{}) ([]byte, error) {
 
-//	{
-//	"pFields": [
-//			"KLLIC_APP_ID",
-//			"KLLIC_APP_IDS",
-//			"KLLIC_CREATION_DATE",
-//			"KLLIC_CUSTOMER_INFO",
-//			"KLLIC_KEY_TYPE",
-//			"KLLIC_LIC_COUNT",
-//			"KLLIC_LICENSE_PERIOD",
-//			"KLLIC_LICINFO",
-//			"KLLIC_LICTYPE_IS_SUBSCRIPTION",
-//			"KLLIC_LIMIT_DATE",
-//			"KLLIC_MAJ_VER",
-//			"KLLIC_NEAREST_EXPIRATION_DATE",
-//			"KLLIC_NHOSTS_ASCURRENT",
-//			"KLLIC_NHOSTS_ASNEXT",
-//			"KLLIC_PROD_NAME",
-//			"KLLIC_PROD_SUITE_ID",
-//			"KLLIC_SERIAL",
-//			"KLLIC_SUBSCRINFO_ENDDATE",
-//			"KLLIC_SUBSCRINFO_ENDDATETYPE",
-//			"KLLIC_SUBSCRINFO_GRACETERM",
-//			"KLLIC_SUBSCRINFO_PROVIDERURL",
-//			"KLLIC_SUBSCRINFO_STATE",
-//			"KLLIC_SUPPORT_INFO",
-//			"KLLICSRV_AUTOKEY",
-//			"KLLICSRV_KEY_INSTALLED",
-//			"KLLICSRV_KEYDATA"
-//	],
-//	"lTimeoutSec": 130
-//	}
-func (lk *LicenseKeys) EnumKeys(ctx context.Context, ekp EnumKeysParams) (*LicenseKeysIterator, []byte, error) {
-	postData, _ := json.Marshal(ekp)
+	postData, _ := json.Marshal(params)
+
 	request, err := http.NewRequest("POST", lk.client.Server+"/api/v1.0/LicenseKeys.EnumKeys",
 		bytes.NewBuffer(postData))
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	licenseKeysIterator := new(LicenseKeysIterator)
-	raw, err := lk.client.Do(ctx, request, &licenseKeysIterator)
 
-	return licenseKeysIterator, raw, err
+	raw, err := lk.client.Do(ctx, request, &v)
+	return raw, err
 }
 
 //KeyDataParams struct
@@ -127,99 +208,32 @@ type PKeyInfo struct {
 	KllicsrvKeydata bool   `json:"KLLICSRV_KEYDATA,omitempty"`
 }
 
-//LicenseData struct
-type LicenseData struct {
-	Data *LData `json:"PxgRetVal,omitempty"`
-}
-
-type LData struct {
-	KllicsrvAutokey                  bool            `json:"KLLICSRV_AUTOKEY,omitempty"`
-	KllicsrvKeydata                  KllicsrvKeydata `json:"KLLICSRV_KEYDATA,omitempty"`
-	KllicsrvKeyEffectiveLicnumberMax int64           `json:"KLLICSRV_KEY_EFFECTIVE_LICNUMBER_MAX,omitempty"`
-	KllicsrvKeyEffectiveLicnumberMin int64           `json:"KLLICSRV_KEY_EFFECTIVE_LICNUMBER_MIN,omitempty"`
-	KllicsrvKeyInstalled             bool            `json:"KLLICSRV_KEY_INSTALLED,omitempty"`
-	KllicAppID                       int64           `json:"KLLIC_APP_ID,omitempty"`
-	KllicAppIDS                      KllicAppIDS     `json:"KLLIC_APP_IDS,omitempty"`
-	KllicCreationDate                Kllic           `json:"KLLIC_CREATION_DATE,omitempty"`
-	KllicCustomerInfo                string          `json:"KLLIC_CUSTOMER_INFO,omitempty"`
-	KllicEffectiveLicensePeriod      int64           `json:"KLLIC_EFFECTIVE_LICENSE_PERIOD,omitempty"`
-	KllicEffectiveLicCount           int64           `json:"KLLIC_EFFECTIVE_LIC_COUNT,omitempty"`
-	KllicKeyType                     int64           `json:"KLLIC_KEY_TYPE,omitempty"`
-	KllicLicensePeriod               int64           `json:"KLLIC_LICENSE_PERIOD,omitempty"`
-	KllicLicinfo                     string          `json:"KLLIC_LICINFO,omitempty"`
-	KllicLicobject1                  int64           `json:"KLLIC_LICOBJECT_1,omitempty"`
-	KllicLicobject102                int64           `json:"KLLIC_LICOBJECT_102,omitempty"`
-	KllicLicobject110                int64           `json:"KLLIC_LICOBJECT_110,omitempty"`
-	KllicLicobject54                 int64           `json:"KLLIC_LICOBJECT_54,omitempty"`
-	KllicLicobject58                 int64           `json:"KLLIC_LICOBJECT_58,omitempty"`
-	KllicLicobject60                 int64           `json:"KLLIC_LICOBJECT_60,omitempty"`
-	KllicLicobject62                 int64           `json:"KLLIC_LICOBJECT_62,omitempty"`
-	KllicLicobject68                 int64           `json:"KLLIC_LICOBJECT_68,omitempty"`
-	KllicLicobject70                 int64           `json:"KLLIC_LICOBJECT_70,omitempty"`
-	KllicLicobject72                 int64           `json:"KLLIC_LICOBJECT_72,omitempty"`
-	KllicLictypeIsSubscription       bool            `json:"KLLIC_LICTYPE_IS_SUBSCRIPTION,omitempty"`
-	KllicLicCount                    int64           `json:"KLLIC_LIC_COUNT,omitempty"`
-	KllicLicIsPendingSubscription    bool            `json:"KLLIC_LIC_IS_PENDING_SUBSCRIPTION,omitempty"`
-	KllicLimitDate                   Kllic           `json:"KLLIC_LIMIT_DATE,omitempty"`
-	KllicMajVer                      string          `json:"KLLIC_MAJ_VER,omitempty"`
-	KllicNhostsAscurrent             int64           `json:"KLLIC_NHOSTS_ASCURRENT,omitempty"`
-	KllicNhostsAsnext                int64           `json:"KLLIC_NHOSTS_ASNEXT,omitempty"`
-	KllicProdName                    string          `json:"KLLIC_PROD_NAME,omitempty"`
-	KllicProdSuiteID                 int64           `json:"KLLIC_PROD_SUITE_ID,omitempty"`
-	KllicSerial                      string          `json:"KLLIC_SERIAL,omitempty"`
-	KllicSubscrinfoEnddatetype       int64           `json:"KLLIC_SUBSCRINFO_ENDDATETYPE,omitempty"`
-	KllicSubscrinfoGraceterm         int64           `json:"KLLIC_SUBSCRINFO_GRACETERM,omitempty"`
-	KllicSubscrinfoProviderurl       string          `json:"KLLIC_SUBSCRINFO_PROVIDERURL,omitempty"`
-	KllicSubscrinfoState             int64           `json:"KLLIC_SUBSCRINFO_STATE,omitempty"`
-	KllicSubscrinfoStatereason       int64           `json:"KLLIC_SUBSCRINFO_STATEREASON,omitempty"`
-	KllicSupportInfo                 string          `json:"KLLIC_SUPPORT_INFO,omitempty"`
-}
-
-type KllicAppIDS struct {
-	Value KLLICAPPIDSValue `json:"value,omitempty"`
-}
-
-type KLLICAPPIDSValue struct {
-	The1105 The1105 `json:"1105,omitempty"`
-}
-
-type The1105 struct {
-	Value map[string]int64 `json:"value,omitempty"`
-}
-
-type Kllic struct {
-	Value string `json:"value,omitempty"`
-}
-
-type KllicsrvKeydata struct {
-	Value KLLICSRVKEYDATAValue `json:"value,omitempty"`
-}
-
-type KLLICSRVKEYDATAValue struct {
-	KllicLicfile *Kllic `json:"KLLIC_LICFILE,omitempty"`
-}
-
-func (u KeyData) ExpirationDate() string {
-	if u.KllicNearestExpirationDate.Value == "" {
-		return "Not Received"
-	} else {
-		return ParseTime(u.KllicNearestExpirationDate.Value)
-	}
-}
-
-//{
-//  			"pKeyInfo" : {
-//    		"KLLIC_SERIAL" : "%s",
-//    		"KLLICSRV_KEYDATA" : true
-//  			}
-//		}
-func (lk *LicenseKeys) GetKeyData(ctx context.Context, kdp KeyDataParams) (*LicenseData, []byte, error) {
-	postData, _ := json.Marshal(kdp)
+//	Get data of a key.
+//
+//	Parameters:
+//	pKeyInfo	(params) container which must contain "KLLIC_SERIAL" attribute to specify the interested license.
+//
+//	For any attribute to query you must put such attribute with any value into the container pKeyInfo.
+//	In particular, if you need the key body then put into
+//	pKeyInfo container the attribute with name "KLLICSRV_KEYDATA" of type (boolean).
+//	If the license key has been uploaded to the Administration Server
+//	so that and Administration Server has license key body then it will be returned in "KLLICSRV_KEYDATA" attribute.
+//	Note that the additional "ExportLicense" access right must be set up to the user under which this call is made.
+//
+//	Returns:
+//	(params) container with the requested key attribute values. See List of license key attributes for attribute names.
+func (lk *LicenseKeys) GetKeyData(ctx context.Context, params KeyDataParams, v interface{}) ([]byte, error) {
+	postData, _ := json.Marshal(params)
 
 	request, err := http.NewRequest("POST", lk.client.Server+"/api/v1.0/LicenseKeys.GetKeyData",
 		bytes.NewBuffer(postData))
 
-	licenseData := new(LicenseData)
-	raw, err := lk.client.Do(ctx, request, &licenseData)
-	return licenseData, raw, err
+	raw, err := lk.client.Do(ctx, request, &v)
+	return raw, err
 }
+
+//TODO func (lk *LicenseKeys) AdjustKey(ctx context.Context, params Params, v interface{})
+//TODO func (lk *LicenseKeys) CheckIfSaasLicenseIsValid(ctx context.Context, params Params, v interface{})
+//TODO func (lk *LicenseKeys) SaasTryToInstall(ctx context.Context, params Params, v interface{})
+//TODO func (lk *LicenseKeys) SaasTryToUninstall(ctx context.Context, params Params, v interface{})
+//TODO func (lk *LicenseKeys) UninstallKey(ctx context.Context, params Params, v interface{})
