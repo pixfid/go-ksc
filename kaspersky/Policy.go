367fe1e28debf5e982260d0466346188790f95d2
pixfid <pixfid@gmail.com>
2020-05-09 13:45:00 +0300

    Add partial support of: Policy - Allows to manage policies, change policies states and acquire policies data SsContents - Access to settings storage.
    Signed-off-by: pixfid <pixfid@gmail.com>

---
+++ kaspersky/Policy.go
@@ -0,0 +1,284 @@
+/*
+ *
+ * 	Copyright (C) 2020  <Semchenko Aleksandr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see <http://www.gnu.org/licenses/>.
+ * /
+ */
+
+package kaspersky
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//	Policy Class Reference
+//
+//	Policies processing. More...
+//
+//	Allows to manage policies, change policies states and acquire policies data
+//
+//	List of all members.
+type Policy struct {
+	client *Client
+}
+
+type PxgValPolicy struct {
+	PxgRetValPolicy PxgRetValPolicy `json:"PxgRetVal"`
+}
+
+type PxgRetValPolicy struct {
+	KlpolAcceptParent          bool   `json:"KLPOL_ACCEPT_PARENT"`
+	KlpolActive                bool   `json:"KLPOL_ACTIVE"`
+	KlpolCreated               Klpol  `json:"KLPOL_CREATED"`
+	KlpolDN                    string `json:"KLPOL_DN"`
+	KlpolForced                bool   `json:"KLPOL_FORCED"`
+	KlpolForceDistrib2Children bool   `json:"KLPOL_FORCE_DISTRIB2CHILDREN"`
+	KlpolGroupID               int64  `json:"KLPOL_GROUP_ID"`
+	KlpolGroupName             string `json:"KLPOL_GROUP_NAME"`
+	KlpolGsynID                int64  `json:"KLPOL_GSYN_ID"`
+	KlpolHideOnSlaves          bool   `json:"KLPOL_HIDE_ON_SLAVES"`
+	KlpolID                    int64  `json:"KLPOL_ID"`
+	KlpolInherited             bool   `json:"KLPOL_INHERITED"`
+	KlpolModified              Klpol  `json:"KLPOL_MODIFIED"`
+	KlpolProduct               string `json:"KLPOL_PRODUCT"`
+	KlpolRoaming               bool   `json:"KLPOL_ROAMING"`
+	KlpolVersion               string `json:"KLPOL_VERSION"`
+}
+
+type Klpol struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+//TODO AddPolicy
+//TODO CopyOrMovePolicy
+
+//	Delete policy.
+//
+//	Makes the the specified policy inactive, and then deletes it.
+//
+//	Parameters:
+//	- nPolicy	(int64) identifier of the policy to delete
+func (pl *Policy) DeletePolicy(ctx context.Context, nPolicy int64) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`{"nPolicy": %d}`, nPolicy))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.DeletePolicy", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := pl.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//	Obtain policies that affect the specified group.
+//
+//	Returns active and roaming policies that affect specified group
+//
+//	Parameters:
+//	- nGroupId	(int64) group id
+//
+//	Returns:
+//	- (array) array, each element is paramParams which contains policy attributes
+//
+//	See also:
+//	List of policy attributes
+func (pl *Policy) GetEffectivePoliciesForGroup(ctx context.Context, nGroupId int64) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`{"nGroupId": %d}`, nGroupId))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.GetEffectivePoliciesForGroup", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := pl.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//
+//	Acquire array of outbreak policies.
+//
+//	Returns the array of outbreak policies
+//
+//	Returns:
+//	- (params) Container with attribute KLPOL_POL_OUTBREAK of type paramArray,
+//	each entry contains two attributes KLPOL_ID and KLPOL_OUTBREAK_MASK
+//
+//	See also:
+//	Policy outbreak attributes
+func (pl *Policy) GetOutbreakPolicies(ctx context.Context) ([]byte, error) {
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.GetOutbreakPolicies", nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := pl.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//	Obtain policies for specified group.
+//
+//	Returns policies located in specified group.
+//
+//	Parameters:
+//	- nGroupId	(int64) value -1 means "all groups"
+//
+//	Returns:
+//	- (array) array, each element is paramParams which contains policy attributes
+//
+//	See also:
+//	List of policy attributes
+func (pl *Policy) GetPoliciesForGroup(ctx context.Context, nGroupId int64) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`{"nGroupId": %d}`, nGroupId))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.GetPoliciesForGroup", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := pl.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//	Acquire policy contents.
+//
+//	Opens settings storage SsContents of the specified policy. The settings storage contains both predefined and product-specific sections.
+//
+//	Parameters:
+//	- nPolicy	(int64) policy id
+//	- nRevisionId	(int64) policy revision id, 0 means 'current policy'
+//	- nLifeTime	(int64) timeout in milliseconds to keep this SsContents object alive, zero means 'default value'
+//
+//	Returns:
+//	- (string) identifier of opened SsContents, must be closed with SsContents::SS_Release
+func (pl *Policy) GetPolicyContents(ctx context.Context, nPolicy, nRevisionId, nLifeTime int64) (*PxgValStr, []byte,
+	error) {
+	postData := []byte(fmt.Sprintf(`{ "nPolicy": %d , "nRevisionId": %d , "nLifeTime": %d }`, nPolicy, nRevisionId,
+		nLifeTime))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.GetPolicyContents", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	pxgValStr := new(PxgValStr)
+	raw, err := pl.client.Do(ctx, request, &pxgValStr)
+	return pxgValStr, raw, err
+}
+
+//	Obtain policy data.
+//
+//	Returns data for specified policy
+//
+//	Parameters:
+//	- nPolicy	(int64) policy id
+//
+//	Returns:
+//	- (params) container with policy attributes ( See List of policy attributes)
+func (pl *Policy) GetPolicyData(ctx context.Context, nPolicy int64) (*PxgValPolicy, []byte, error) {
+	postData := []byte(fmt.Sprintf(`{"nPolicy": %d}`, nPolicy))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.GetPolicyData", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	pxgValPolicy := new(PxgValPolicy)
+	raw, err := pl.client.Do(ctx, request, &pxgValPolicy)
+	return pxgValPolicy, raw, err
+}
+
+//	Make policy active or inactive.
+//
+//	Make the specified policy active or inactive
+//
+//	Parameters:
+//	- nPolicy	(int64) policy id to activate
+//	- bActive	(bool) true to make the specified inactive or roaming policy active (
+//	returns false if the policy is already active),
+//	false to make the specified active or roaming policy inactive
+//	(returns false if the policy is already inactive)
+//
+//	Returns:
+//	- (boolean) true if policy was successfully set to active or false otherwise
+//
+//	See also:
+//	List of policy attributes
+func (pl *Policy) MakePolicyActive(ctx context.Context, nPolicy int64, bActive bool) (*PxgValPolicy, []byte, error) {
+	postData := []byte(fmt.Sprintf(`{"nPolicy": %d, "bActive": %v}`, nPolicy, bActive))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.MakePolicyActive", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	pxgValPolicy := new(PxgValPolicy)
+	raw, err := pl.client.Do(ctx, request, &pxgValPolicy)
+	return pxgValPolicy, raw, err
+}
+
+//	Make policy roaming.
+//
+//	Make the specified policy roaming
+//
+//	Parameters:
+//	- nPolicy	(int64) active or inactive policy id
+//
+//	Returns:
+//	- (bool) false if the policy is already roaming
+//
+//	See also:
+//	KLPOL_ROAMING
+//	List of policy attributes
+func (pl *Policy) MakePolicyRoaming(ctx context.Context, nPolicy int64) (*PxgValPolicy, []byte, error) {
+	postData := []byte(fmt.Sprintf(`{"nPolicy": %d}`, nPolicy))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.MakePolicyRoaming", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	pxgValPolicy := new(PxgValPolicy)
+	raw, err := pl.client.Do(ctx, request, &pxgValPolicy)
+	return pxgValPolicy, raw, err
+}
+
+//	Revert policy to its older version.
+//
+//	Replaces the specified policy nPolicy by its revision (older version) nRevisionId
+//
+//	Parameters:
+//	- nPolicy	(int64) id of policy to revert
+//	- nRevisionId	(int64) id of policy revision
+func (pl *Policy) RevertPolicyToRevision(ctx context.Context, nPolicy, nRevisionId int64) (*PxgValPolicy, []byte,
+	error) {
+	postData := []byte(fmt.Sprintf(`{"nPolicy": %d, "nRevisionId": %d}`, nPolicy, nRevisionId))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/Policy.RevertPolicyToRevision", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	pxgValPolicy := new(PxgValPolicy)
+	raw, err := pl.client.Do(ctx, request, &pxgValPolicy)
+	return pxgValPolicy, raw, err
+}
+
+//TODO SetOutbreakPolicies
+//TODO UpdatePolicyData
---
+++ kaspersky/SsContents.go
@@ -0,0 +1,199 @@
+/*
+ *
+ * 	Copyright (C) 2020  <Semchenko Aleksandr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see <http://www.gnu.org/licenses/>.
+ * /
+ */
+
+package kaspersky
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//	SsContents Class Reference
+//	Access to settings storage. More...
+//
+//	List of all members.
+type SsContents struct {
+	client *Client
+}
+
+//TODO Ss_Add
+
+//	Save changes
+//
+//	Saves changes made by methods:
+//
+//	Ss_Update,
+//	Ss_Add,
+//	Ss_Replace,
+//	Ss_Clear,
+//	Ss_Delete,
+//	Ss_CreateSection,
+//	Ss_DeleteSection
+//
+//	Parameters:
+//	- wstrID	(string) identifier of opened SsContents
+func (sc *SsContents) Ss_Apply(ctx context.Context, wstrID string) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`	{ "wstrID": "%s"}`, wstrID))
+
+	request, err := http.NewRequest("POST", sc.client.Server+"/api/v1.0/SsContents.Ss_Apply", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := sc.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//TODO Ss_Clear
+
+//	Create section in settings storage.
+//
+//	Creates empty section in settings storage
+//
+//	Changes are not saved until method Ss_Apply is called. Unsaved data is not available by methods Ss_Read and SS_GetNames.
+//
+//	Parameters:
+//	- wstrID	(string) identifier of opened SsContents
+//	- wstrProduct	(string) product name string, non-empty string, not longer than 31 character,
+//	and cannot contain characters /\:*?"<>.
+//	- wstrVersion	(string) version string, non-empty string, not longer than 31 character,
+//	and cannot contain characters /\:*?"<>.
+//	- wstrSection	(string) section name string, non-empty string, not longer than 31 character,
+//	and cannot contain characters /\:*?"<>.
+func (sc *SsContents) Ss_CreateSection(ctx context.Context, wstrID, wstrProduct, wstrVersion, wstrSection string) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`
+	{ 
+		"wstrID": "%s",
+		"wstrProduct": "%s",
+		"wstrVersion": "%s",
+		"wstrSection": "%s" }`, wstrID, wstrProduct, wstrVersion, wstrSection))
+
+	request, err := http.NewRequest("POST", sc.client.Server+"/api/v1.0/SsContents.Ss_CreateSection", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := sc.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//TODO Ss_Delete
+
+func (sc *SsContents) Ss_DeleteSection(ctx context.Context, wstrID, wstrProduct, wstrVersion, wstrSection string) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`
+	{ 
+		"wstrID": "%s",
+		"wstrProduct": "%s",
+		"wstrVersion": "%s",
+		"wstrSection": "%s" }`, wstrID, wstrProduct, wstrVersion, wstrSection))
+
+	request, err := http.NewRequest("POST", sc.client.Server+"/api/v1.0/SsContents.Ss_DeleteSection", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := sc.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//	Enumerate contents of settings storage
+//
+//	Retrieves list of sections.
+//
+//	Parameters:
+//	- wstrID	(string) identifier of opened SsContents
+//	- wstrProduct	(string) product name string, string, not longer than 31 character,
+//	and cannot contain characters /\:*?"<>. Empty string means that list of products is needed.
+//	- wstrVersion	(string) version string, string, not longer than 31 character,
+//	and cannot contain characters /\:*?"<>. Empty string means that list of versions is needed.
+//
+//	Returns:
+//	- (array) list of section if both wstrProduct and wstrVersion specified,
+//	list of version if only wstrProduct is specified, list of product is wstrProduct is not specified
+func (sc *SsContents) SS_GetNames(ctx context.Context, wstrID, wstrProduct, wstrVersion string) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`{ "wstrID": "%s" , "wstrProduct": "%s" , "nLifeTime": "%s"}`,
+		wstrID,
+		wstrProduct,
+		wstrVersion))
+
+	request, err := http.NewRequest("POST", sc.client.Server+"/api/v1.0/SsContents.SS_GetNames", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := sc.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//	Read data from settings storage.
+//
+//	Reads saved data from the specified section of settings storage.
+//
+//	Parameters:
+//	- wstrID	(string) identifier of opened SsContents. Use PolicyProfiles::GetEffectivePolicyContents to get it
+//	- wstrProduct	(string) product name string (see Settings storage section parameters)
+//	- wstrVersion	(string) version string (see Settings storage section parameters)
+//	- wstrSection	(string) section name string (see Settings storage section parameters)
+//
+//	Returns:
+//	- (params) data from the specified section
+//
+//	See also:
+//	Settings storage section parameters
+func (sc *SsContents) Ss_Read(ctx context.Context, wstrID, wstrProduct, wstrVersion, wstrSection string) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`
+	{ 
+		"wstrID": "%s",
+		"wstrProduct": "%s",
+		"wstrVersion": "%s",
+		"wstrSection": "%s" }`, wstrID, wstrProduct, wstrVersion, wstrSection))
+
+	request, err := http.NewRequest("POST", sc.client.Server+"/api/v1.0/SsContents.Ss_Read", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := sc.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//	Close opened SsContents
+//
+//	Closes opened SsContents and releases associated server resources.
+//	After calling this method wstrID is not longer valid.
+//
+//	Parameters:
+//	- wstrID	(string) identifier of opened SsContents
+func (pl *SsContents) Ss_Release(ctx context.Context, wstrID string) ([]byte, error) {
+	postData := []byte(fmt.Sprintf(`{"wstrID": "%s"}`, wstrID))
+
+	request, err := http.NewRequest("POST", pl.client.Server+"/api/v1.0/SsContents.Ss_Release", bytes.NewBuffer(postData))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	raw, err := pl.client.Do(ctx, request, nil)
+	return raw, err
+}
+
+//TODO Ss_Replace
+//TODO Ss_Update
