88f5ae0357bd9fd8fd4fd7424fcb7031aa9978cd
pixfid <pixfid@gmail.com>
2020-05-25 23:44:02 +0300

    Improve VServers service
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/VServers.go
+++ kaspersky/VServers.go
@@ -59,24 +59,53 @@ func (vs *VServers) GetVServers(ctx context.Context, lParentGroup int64) ([]byte
 	return raw, err
 }
 
+//VServerInfo struct
 type VServerInfo struct {
 	VServer *VServer `json:"PxgRetVal,omitempty"`
 }
 
+//	VServer struct
 type VServer struct {
-	KlvsrvCreated            *KlvsrvCreated `json:"KLVSRV_CREATED,omitempty"`
-	KlvsrvDN                 *string        `json:"KLVSRV_DN,omitempty"`
-	KlvsrvEnabled            *bool          `json:"KLVSRV_ENABLED,omitempty"`
-	KlvsrvGroups             *int64         `json:"KLVSRV_GROUPS,omitempty"`
-	KlvsrvGrp                *int64         `json:"KLVSRV_GRP,omitempty"`
-	KlvsrvHstUid             *string        `json:"KLVSRV_HST_UID,omitempty"`
-	KlvsrvID                 *int64         `json:"KLVSRV_ID,omitempty"`
-	KlvsrvLicEnabled         *bool          `json:"KLVSRV_LIC_ENABLED,omitempty"`
-	KlvsrvNewHostsProhibited *bool          `json:"KLVSRV_NEW_HOSTS_PROHIBITED,omitempty"`
-	KlvsrvSuper              *int64         `json:"KLVSRV_SUPER,omitempty"`
-	KlvsrvTooMuchHosts       *bool          `json:"KLVSRV_TOO_MUCH_HOSTS,omitempty"`
-	KlvsrvUid                *string        `json:"KLVSRV_UID,omitempty"`
-	KlvsrvUnassigned         *int64         `json:"KLVSRV_UNASSIGNED,omitempty"`
+	//Creation time.
+	KlvsrvCreated *KlvsrvCreated `json:"KLVSRV_CREATED,omitempty"`
+
+	//Virtual server display name.
+	KlvsrvDN *string `json:"KLVSRV_DN,omitempty"`
+
+	//If the specified virtual server is enabled.
+	KlvsrvEnabled *bool `json:"KLVSRV_ENABLED,omitempty"`
+
+	//Id of virtual server's group "Managed computers".
+	KlvsrvGroups *int64 `json:"KLVSRV_GROUPS,omitempty"`
+
+	//Id of parent group
+	KlvsrvGrp *int64 `json:"KLVSRV_GRP,omitempty"`
+
+	//Host name of the virtual server (See KLHST_WKS_HOSTNAME attribute )
+	KlvsrvHstUid *string `json:"KLVSRV_HST_UID,omitempty"`
+
+	//Virtual server ID
+	KlvsrvID *int64 `json:"KLVSRV_ID,omitempty"`
+
+	//Allow automatic deployment of license keys from the virtual Server to its devices.
+	KlvsrvLicEnabled *bool `json:"KLVSRV_LIC_ENABLED,omitempty"`
+
+	//New managed hosts cannot be added to the VS due to the limitation violation
+	//(see LP_VS_MaxCountOfHosts), read-only attribute.
+	KlvsrvNewHostsProhibited *bool `json:"KLVSRV_NEW_HOSTS_PROHIBITED,omitempty"`
+
+	//Id of virtual server's group "Master server".
+	KlvsrvSuper *int64 `json:"KLVSRV_SUPER,omitempty"`
+
+	//Number of managed hosts connected to a VS has already reached 10% of the limit
+	//(see LP_VS_MaxCountOfHosts) , read-only attribute.
+	KlvsrvTooMuchHosts *bool `json:"KLVSRV_TOO_MUCH_HOSTS,omitempty"`
+
+	//Virtual server name - a globally unique string
+	KlvsrvUid *string `json:"KLVSRV_UID,omitempty"`
+
+	//Id of virtual server's group "Unassigned computers".
+	KlvsrvUnassigned *int64 `json:"KLVSRV_UNASSIGNED,omitempty"`
 }
 
 type KlvsrvCreated struct {
@@ -145,8 +174,8 @@ func (vs *VServers) GetPermissions(ctx context.Context, lVServer int64) ([]byte,
 }
 
 //HostInfoParams struct
-type VServerParams struct {
-	LVServer       int64    `json:"lVServer"`
+type VServerInfoParams struct {
+	LVServer       int64    `json:"lVServer,omitempty"`
 	PFields2Return []string `json:"pFields2Return"`
 }
 
@@ -155,34 +184,24 @@ type VServerParams struct {
 //	Returns info about the specified virtual server
 //
 //	Parameters:
-//	- lVServer	(int64) virtual server id
-//	- pFields2Return	(array) attributes "KLVSRV_*" to acquire (see List of virtual server attributes).
+//	- params VServerInfoParams
 //
 //	Returns:
 //	- (params) a container containing attributes "KLVSRV_*" (see List of virtual server attributes)
-func (vs *VServers) GetVServerInfo(ctx context.Context, lVServer int64) ([]byte, error) {
-	v := VServerParams{LVServer: lVServer, PFields2Return: []string{
-		"KLVSRV_CUSTOM_INFO",
-		"KLVSRV_ID",
-		"KLVSRV_UID",
-		"KLVSRV_GRP",
-		"KLVSRV_DN",
-		"KLVSRV_GROUPS",
-		"KLVSRV_SUPER",
-		"KLVSRV_UNASSIGNED",
-		"KLVSRV_ENABLED",
-		"KLVSRV_LIC_ENABLED",
-		"KLVSRV_HST_UID",
-		"KLVSRV_CREATED",
-	}}
-	postData, _ := json.Marshal(v)
-	request, err := http.NewRequest("POST", vs.client.Server+"/api/v1.0/VServers.GetVServerInfo", bytes.NewBuffer(postData))
+func (vs *VServers) GetVServerInfo(ctx context.Context, params VServerInfoParams) (*VServerInfo, []byte, error) {
+	postData, err := json.Marshal(params)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	raw, err := vs.client.Do(ctx, request, nil)
-	return raw, err
+	request, err := http.NewRequest("POST", vs.client.Server+"/api/v1.0/VServers.GetVServerInfo", bytes.NewBuffer(postData))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	vServerInfo := new(VServerInfo)
+	raw, err := vs.client.Do(ctx, request, &vServerInfo)
+	return vServerInfo, raw, err
 }
 
 //	Moves specified virtual server.
@@ -225,18 +244,49 @@ func (vs *VServers) RecallCertAndCloseConnections(ctx context.Context, lVServer 
 	return raw, err
 }
 
-//TODO ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+//UpdateVServerInfoParams struct
+type UpdateVServerInfoParams struct {
+	LVServer *int64  `json:"lVServer,omitempty"`
+	VSInfo   *VSInfo `json:"pInfo,omitempty"`
+}
+
+//VSInfo struct
+type VSInfo struct {
+	//Virtual server display name.
+	KlvsrvDN *string `json:"KLVSRV_DN,omitempty"`
+
+	//If the specified virtual server is enabled.
+	KlvsrvEnabled *bool `json:"KLVSRV_ENABLED,omitempty"`
+
+	//Allow automatic deployment of license keys from the virtual Server to its devices.
+	KlvsrvLicEnabled *bool `json:"KLVSRV_LIC_ENABLED,omitempty"`
+
+	//Creation time.
+	KlvsrvCreated *KlvsrvCreated `json:"KLVSRV_CREATED,omitempty"`
+
+	//Custom info XML.
+	KlvsrvCustomInfo *string `json:"KLVSRV_CUSTOM_INFO,omitempty"`
+
+	//New managed hosts cannot be added to the VS due to the limitation violation
+	//(see LP_VS_MaxCountOfHosts), read-only attribute.
+	KlvsrvNewHostsProhibited *bool `json:"KLVSRV_NEW_HOSTS_PROHIBITED,omitempty"`
+
+	//Number of managed hosts connected to a VS has already reached 10% of the limit
+	//(see LP_VS_MaxCountOfHosts) , read-only attribute.
+	KlvsrvTooMuchHosts *bool `json:"KLVSRV_TOO_MUCH_HOSTS,omitempty"`
+}
 
 //	Modify virtual server attributes.
 //
 //	Modifies attributes of the specified virtual server
 //
 //	Parameters:
-//	- lVServer	(int64) virtual server id
-//	- pInfo	(params) a container containing no-read-only attributes "KLVSRV_*" to chnage (
-//	see List of virtual server attributes). Following attributes may be specified: "KLVSRV_DN"
-func (vs *VServers) UpdateVServerInfo(ctx context.Context, lVServer int64, params interface{}) ([]byte, error) {
-	postData := []byte(fmt.Sprintf(`{"lVServer": %d}`, lVServer))
+//	- params UpdateVServerInfoParams
+func (vs *VServers) UpdateVServerInfo(ctx context.Context, params UpdateVServerInfoParams) ([]byte, error) {
+	postData, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
 	request, err := http.NewRequest("POST", vs.client.Server+"/api/v1.0/VServers.UpdateVServerInfo", bytes.NewBuffer(postData))
 	if err != nil {
 		return nil, err
@@ -251,12 +301,13 @@ func (vs *VServers) UpdateVServerInfo(ctx context.Context, lVServer int64, param
 //	Sets ACL for the specified virtual server
 //
 //	Parameters:
-//	- lVServer	(int64) virtual server id
-//	- pPermissions	(params) ACL, see Permissions structure
-//	- bProtection	(bool) if true checks if the user does not denies access to the server to itself
-func (vs *VServers) SetPermissions(ctx context.Context, lVServer int64, params interface{}, bProtection bool) ([]byte,
-	error) {
-	postData := []byte(fmt.Sprintf(`{"lVServer": %d}`, lVServer))
+//	- params interface{}
+func (vs *VServers) SetPermissions(ctx context.Context, params interface{}) ([]byte, error) {
+	postData, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
 	request, err := http.NewRequest("POST", vs.client.Server+"/api/v1.0/VServers.SetPermissions", bytes.NewBuffer(postData))
 	if err != nil {
 		return nil, err
