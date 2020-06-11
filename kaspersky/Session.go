655ca8f1c67e973da363003b871a6fa8f9a02e0f
pixfid <pixfid@gmail.com>
2020-06-12 01:23:11 +0300

    Add Authenticated session support
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/Session.go
+++ kaspersky/Session.go
@@ -25,7 +25,9 @@
 package kaspersky
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"net/http"
 )
 
@@ -104,13 +106,72 @@ func (s *Session) EndSession(ctx context.Context) ([]byte, error) {
 //	Authenticated session
 func (s *Session) StartSession(ctx context.Context) (*PxgValStr, []byte, error) {
 	request, err := http.NewRequest("POST", s.client.Server+"/api/v1.0/Session.StartSession", nil)
+
 	if err != nil {
 		return nil, nil, err
 	}
+
+	request.Header.Set("Authorization", "KSCBasic user=\""+s.client.UserName+"\", pass=\""+s.client.Password+"\"")
+	request.Header.Set("X-KSC-VServer", s.client.VServerName)
 
 	pxgValStr := new(PxgValStr)
 	raw, err := s.client.Do(ctx, request, &pxgValStr)
 	return pxgValStr, raw, err
 }
 
-//TODO func (s *Session) CreateBlob(ctx context.Context, params interface{}) ([]byte, error) {return nil, nil}
+//	Create blob with connection parameters for the klsctunnel utility.
+//
+//	Create blob with connection parameters for the klsctunnel utility, including target address and one-time authentication token. See Tunnels creation
+//
+//	Creates blob for current security context.
+//
+//	Parameters:
+//	- pParams	(params) Additional params May contain:
+//	|- KLCST_TARGET_HOST_NAME (paramString) - target application location. Empty value means "localhost".
+//	Non-empty values require "Desktop sharing session" privilege;
+//	|- KLCST_TARGET_PORT (paramInt) - target application port. Access to 3389 port requires "RDP session" privilege;
+//	|- KLCST_LOCATIONS (array) of (params) gateway nodes locations; See CgwHelper
+//	|- KLCST_USER_COMMAND (paramInt) - User command to execute on gw connection. Possible values:
+//		0 - Unknown command
+//		1 - Remote desktop connection
+//		2 - Windows desktop sharing
+//	|- KLCST_RDS_TICKET (paramString) - Windows desktop sharing ticket for Windows desktop sharing.
+//	See KLCST_USER_COMMAND;
+//	|- KLCST_RDS_PASSWORD (paramString) - Windows desktop sharing ticket password for Windows desktop sharing.
+//	See KLCST_USER_COMMAND
+//
+//	To create blob for RDP one should specify following values in the pParams container:
+//
+//	- KLCST_TARGET_PORT = 3389
+//	- KLCST_LOCATIONS = `path to the target host as array of locations,
+//	see CgwHelper::GetNagentLocation and CgwHelper::GetSlaveServerLocation methods`
+//	- KLCST_USER_COMMAND = 1
+//To create blob for WDS one should specify following values in the pParams container:
+//
+//	- KLCST_TARGET_HOST_NAME = `value of the wstrHostNameOrIpAddr output parameter of the NagRemoteScreen
+//	::GetDataForTunnel method`
+//	- KLCST_TARGET_PORT = `value of the nHostPortNumber output parameter of the NagRemoteScreen::GetDataForTunnel
+//	method`
+//	- KLCST_LOCATIONS = `path to the target host as array of locations`,
+//	see CgwHelper::GetNagentLocation and CgwHelper::GetSlaveServerLocation methods
+//	- KLCST_USER_COMMAND = 2
+//	- KLCST_RDS_TICKET = `value of the wstrTicket output parameter of the NagRemoteScreen::GetWdsData method`
+//	- KLCST_RDS_PASSWORD = `value of the wstrPassword output parameter of the NagRemoteScreen::GetWdsData method`
+//
+//	Returns:
+//	- Base 64 encoded connection blob.
+func (s *Session) CreateBlob(ctx context.Context, params interface{}) ([]byte, error) {
+	postData, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest("POST", s.client.Server+"/api/v1.0/Session.CreateBlob",
+		bytes.NewBuffer(postData))
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := s.client.Do(ctx, request, nil)
+	return raw, err
+}
