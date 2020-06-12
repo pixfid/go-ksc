d2be6f1de13a0e6e78527b478aa99eaadee3750b
pixfid <pixfid@gmail.com>
2020-06-12 13:08:43 +0300

    Improved HostTagsRulesApi
    Add UpdateRule - Adds/Updates host automatic tagging rule.
    
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/HostTagsRulesApi.go
+++ kaspersky/HostTagsRulesApi.go
@@ -74,14 +74,14 @@ type HostTagsRulesParams struct {
 //	- ppRules (params) contains following attributes:
 //	"KLHST_HTR_Rules" - host automatic tagging rules (paramArray|paramParams)
 //	list of attributes that are specified in pFields2ReturnArray
-func (hs *HostTagsRulesApi) GetRules(ctx context.Context, params HostTagsRulesParams) ([]byte, error) {
+func (htra *HostTagsRulesApi) GetRules(ctx context.Context, params HostTagsRulesParams) ([]byte, error) {
 	postData, _ := json.Marshal(params)
-	request, err := http.NewRequest("POST", hs.client.Server+"/api/v1.0/HostTagsRulesApi.GetRules", bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", htra.client.Server+"/api/v1.0/HostTagsRulesApi.GetRules", bytes.NewBuffer(postData))
 	if err != nil {
 		return nil, err
 	}
 
-	raw, err := hs.client.Do(ctx, request, nil)
+	raw, err := htra.client.Do(ctx, request, nil)
 	return raw, err
 }
 
@@ -94,14 +94,14 @@ func (hs *HostTagsRulesApi) GetRules(ctx context.Context, params HostTagsRulesPa
 //
 //	Returns:
 //	- (params) object containing attributes of specified rule, see List of host automatic tagging rule attributes.
-func (hs *HostTagsRulesApi) GetRule(ctx context.Context, szwTagValue string) ([]byte, error) {
+func (htra *HostTagsRulesApi) GetRule(ctx context.Context, szwTagValue string) ([]byte, error) {
 	postData := []byte(fmt.Sprintf(`{"szwTagValue": "%s"}`, szwTagValue))
-	request, err := http.NewRequest("POST", hs.client.Server+"/api/v1.0/HostTagsRulesApi.GetRule", bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", htra.client.Server+"/api/v1.0/HostTagsRulesApi.GetRule", bytes.NewBuffer(postData))
 	if err != nil {
 		return nil, err
 	}
 
-	raw, err := hs.client.Do(ctx, request, nil)
+	raw, err := htra.client.Do(ctx, request, nil)
 	return raw, err
 }
 
@@ -118,15 +118,15 @@ func (hs *HostTagsRulesApi) GetRule(ctx context.Context, szwTagValue string) ([]
 //	- szwTagValue	(string). tag of the rule. rule id
 //	- [out]	wstrActionGuid	(string) id of asynchronous operation, to get status use AsyncActionStateChecker.
 //	CheckActionState
-func (hs *HostTagsRulesApi) ExecuteRule(ctx context.Context, szwTagValue string) (*WActionGUID, []byte, error) {
+func (htra *HostTagsRulesApi) ExecuteRule(ctx context.Context, szwTagValue string) (*WActionGUID, []byte, error) {
 	postData := []byte(fmt.Sprintf(`{"szwTagValue": "%s"}`, szwTagValue))
-	request, err := http.NewRequest("POST", hs.client.Server+"/api/v1.0/HostTagsRulesApi.ExecuteRule", bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", htra.client.Server+"/api/v1.0/HostTagsRulesApi.ExecuteRule", bytes.NewBuffer(postData))
 	if err != nil {
 		return nil, nil, err
 	}
 
 	wActionGUID := new(WActionGUID)
-	raw, err := hs.client.Do(ctx, request, &wActionGUID)
+	raw, err := htra.client.Do(ctx, request, &wActionGUID)
 	return wActionGUID, raw, err
 }
 
@@ -136,14 +136,14 @@ func (hs *HostTagsRulesApi) ExecuteRule(ctx context.Context, szwTagValue string)
 //
 //	Parameters:
 //	- wstrActionGuid	(string). id of asynchronous operation that has been started earlier
-func (hs *HostTagsRulesApi) CancelAsyncAction(ctx context.Context, wstrActionGuid string) ([]byte, error) {
+func (htra *HostTagsRulesApi) CancelAsyncAction(ctx context.Context, wstrActionGuid string) ([]byte, error) {
 	postData := []byte(fmt.Sprintf(`{"wstrActionGuid": "%s"}`, wstrActionGuid))
-	request, err := http.NewRequest("POST", hs.client.Server+"/api/v1.0/HostTagsRulesApi.CancelAsyncAction", bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", htra.client.Server+"/api/v1.0/HostTagsRulesApi.CancelAsyncAction", bytes.NewBuffer(postData))
 	if err != nil {
 		return nil, err
 	}
 
-	raw, err := hs.client.Do(ctx, request, nil)
+	raw, err := htra.client.Do(ctx, request, nil)
 	return raw, err
 }
 
@@ -151,15 +151,96 @@ func (hs *HostTagsRulesApi) CancelAsyncAction(ctx context.Context, wstrActionGui
 //
 //	Parameters:
 //	- szwTagValue	(string). tag of the rule. rule id
-func (hs *HostTagsRulesApi) DeleteRule(ctx context.Context, szwTagValue string) ([]byte, error) {
+func (htra *HostTagsRulesApi) DeleteRule(ctx context.Context, szwTagValue string) ([]byte, error) {
 	postData := []byte(fmt.Sprintf(`{"szwTagValue": "%s"}`, szwTagValue))
-	request, err := http.NewRequest("POST", hs.client.Server+"/api/v1.0/HostTagsRulesApi.DeleteRule", bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", htra.client.Server+"/api/v1.0/HostTagsRulesApi.DeleteRule", bytes.NewBuffer(postData))
 	if err != nil {
 		return nil, err
 	}
 
-	raw, err := hs.client.Do(ctx, request, nil)
+	raw, err := htra.client.Do(ctx, request, nil)
 	return raw, err
 }
 
-//TODO HostTagsRulesApi.UpdateRule
+//UpdateRuleParams struct using in HostTagsRulesApi.UpdateRule
+type UpdateRuleParams struct {
+	//tag of the rule. rule id
+	SzwTagValue string `json:"szwTagValue"`
+
+	//object containing rule attributes, see List of host automatic tagging rule attributes.
+	PRuleInfo PRuleInfo `json:"pRuleInfo"`
+}
+
+type PRuleInfo struct {
+	//Rule display name.
+	KlhstHtrDN string `json:"KLHST_HTR_DN"`
+
+	//Whether rule is turned on.
+	KLHSTHTREnabled bool `json:"KLHST_HTR_Enabled"`
+
+	//Tag value that will be set by rule. It is rule identifier.
+	KLHSTHTRTagValue string `json:"KLHST_HTR_TagValue"`
+
+	//	Host filtering expression (see Search filter syntax).
+	//
+	//	Don't use filtering by fields other then listed below.
+	//
+	//	- KLHST_WKS_FROM_UNASSIGNED
+	//	- KLHST_NAG_INSTID
+	//	- KLHST_WKS_STATUS
+	//	- KLHST_WKS_WINHOSTNAME
+	//	- KLHST_WKS_WINDOMAIN
+	//	- KLHST_WKS_DNSNAME
+	//	- KLHST_WKS_DNSDOMAIN
+	//	- KLHST_WKS_IP_LONG
+	//	- KLHST_WKS_CONNECT_IP_LONG
+	//	- KLHST_WKS_CTYPE
+	//	- KLHST_WKS_PTYPE
+	//	- KLHST_WKS_OS_VER_MAJOR
+	//	- KLHST_WKS_OS_VER_MINOR
+	//	- KLHST_WKS_OSSP_VER_MAJOR
+	//	- KLHST_WKS_CPU_ARCH
+	//	- KLDPNS_ID
+	//	- KLHST_WKS_GROUPID
+	//	- KLHST_AD_ORGUNIT
+	//	- KLHST_AD_ORGUNIT_GP
+	//	- KLHST_AD_GROUP
+	//	- KLHST_MOB_HAS_OWNER_CERT
+	//	- HST_VM_TYPE
+	//	- HST_VM_VDI
+	//	- KLHST_INVENTORY_PRODUCT_NAME
+	//	- KLHST_INVENTORY_PRODUCT_DISPLAY_VERSION
+	//	- KLHST_INVENTORY_PRODUCT_PUBLISHER
+	KLHSTHTRQuery string `json:"KLHST_HTR_Query"`
+
+	//Any data associated with rule. It is not analyzed by the Administration Server
+	KLHSTHTRCustom KLHSTHTRCustom `json:"KLHST_HTR_Custom"`
+}
+
+type KLHSTHTRCustom struct {
+	//type "params"
+	Type string `json:"type"`
+
+	//Any data associated with rule. It is not analyzed by the Administration Server
+	CustomValue CustomValue `json:"value"`
+}
+
+type CustomValue struct{}
+
+//	Adds/Updates host automatic tagging rule.
+//
+//	Parameters:
+//	- szwTagValue	(wstring). tag of the rule. rule id
+//	- pRuleInfo	(params) object containing rule attributes, see List of host automatic tagging rule attributes.
+//	Following attributes are required:
+//	- "KLHST_HTR_DN"
+func (htra *HostTagsRulesApi) UpdateRule(ctx context.Context, params UpdateRuleParams) ([]byte, error) {
+	postData, _ := json.Marshal(params)
+	request, err := http.NewRequest("POST", htra.client.Server+"/api/v1.0/HostTagsRulesApi.UpdateRule", bytes.NewBuffer(postData))
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err := htra.client.Do(ctx, request, nil)
+	return raw, err
+}
