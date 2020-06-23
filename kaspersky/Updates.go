fe9778807b2ee460a4c44d876c075df5670b5319
pixfid <pixfid@gmail.com>
2020-06-23 22:52:38 +0300

    refactor\cleanup Updates
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/Updates.go
+++ kaspersky/Updates.go
@@ -27,26 +27,59 @@ package kaspersky
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"net/http"
 )
 
-//	Updates Class Reference
-//
-//	Updates processing..
-//
-//	List of all members:
+// Updates Updates processing
 type Updates service
 
-//	Get available updates info.
-//
-//	Parameters:
-//	- strLocalization	(string) - localization to be used for compoments titles;
-//	can be one of "ru", "en", "fr", "de"; for other values "en" localization will be used.
-//	Return:
-//	- pAvailableUpdateComps	(params) - see Well-known retranslated update components list
-func (upd *Updates) GetAvailableUpdatesInfo(ctx context.Context, strLocalization string) ([]byte, error) {
+// AvailableUpdates containing well-known retranslated update components list
+type AvailableUpdates struct {
+	PAvailableUpdateComps *PAvailableUpdateComps `json:"pAvailableUpdateComps,omitempty"`
+}
+
+// PAvailableUpdateComps
+type PAvailableUpdateComps struct {
+	KLUpdates []KLUpdate `json:"KLUpdates"`
+}
+
+// KLUpdate
+type KLUpdate struct {
+	Type *string `json:"type,omitempty"`
+	// KLUpdateValue containing well-known retranslated update components list,
+	// which can be used for update retranslation task tuning
+	KLUpdateValue *KLUpdateValue `json:"value,omitempty"`
+}
+
+type KLUpdateValue struct {
+	// AppID Application identity, only anti-spam components, e.g.: "1184"
+	AppID *string `json:"AppId,omitempty"`
+
+	// AppSuperID Application family identity, for anti-spam components only, possible values:
+	//	0 - Unknown
+	//	1 - AntiSpam Personal 1.0
+	//	2 - AntiSpam Personal 1.1
+	//	3 - Security for SMTPGateway 5.5
+	//	4 - Security for Exchange 2003
+	//	5 - AntiSpam 3.0
+	AppSuperID *string `json:"AppSuperId,omitempty"`
+
+	// CompID Component internal name, e.g.: "KDB"
+	CompID *string `json:"CompId,omitempty"`
+
+	// Descr Component description
+	Descr *string `json:"Descr,omitempty"`
+
+	// Name Component title
+	Name *string `json:"Name,omitempty"`
+}
+
+// GetAvailableUpdatesInfo Get available updates info.
+// strLocalization can be one of "ru", "en", "fr", "de"; for other values "en" localization will be used.
+func (upd *Updates) GetAvailableUpdatesInfo(ctx context.Context, strLocalization string) (*AvailableUpdates, error) {
 	postData := []byte(fmt.Sprintf(`{"strLocalization": "%s"}`, strLocalization))
 	request, err := http.NewRequest("POST", upd.client.Server+"/api/v1.0/Updates.GetAvailableUpdatesInfo",
 		bytes.NewBuffer(postData))
@@ -54,60 +87,82 @@ func (upd *Updates) GetAvailableUpdatesInfo(ctx context.Context, strLocalization
 		return nil, err
 	}
 
-	raw, err := upd.client.Do(ctx, request, nil)
-	return raw, err
+	availableUpdates := new(AvailableUpdates)
+	_, err = upd.client.Do(ctx, request, &availableUpdates)
+	return availableUpdates, err
 }
 
-//	Returns a list of retransmissions.
-//
-//	Parameters:
-//	- pFilter	(array) array of strings, contains a list of bundle identities (
-//	KLUPDSRV_BUNDLE_ID) for which you want to return data. If NULL then a list of all retranslated updates will be returned, but the only one returned attribute for all updates is KLUPDSRV_BUNDLE_ID.
-//
-//	Returns:
-//(array) result, see List of attributes of a retranslated update component bundle
-func (upd *Updates) GetUpdatesInfo(ctx context.Context) ([]byte, error) {
-	postData := []byte(fmt.Sprintf(`{"pFilter" : ["KAS20EXCH"]}`))
-	request, err := http.NewRequest("POST", upd.client.Server+"/api/v1.0/Updates.GetUpdatesInfo",
-		bytes.NewBuffer(postData))
+// UpdatesInfoParams
+type UpdatesInfoParams struct {
+	PFilter []string `json:"pFilter"`
+}
+
+// UpdatesInfos
+type UpdatesInfos struct {
+	UpdatesInfo []UpdatesInfo `json:"PxgRetVal"`
+}
+
+// UpdatesInfo
+type UpdatesInfo struct {
+	Type             *string           `json:"type,omitempty"`
+	UpdatesInfoValue *UpdatesInfoValue `json:"value,omitempty"`
+}
+
+// UpdatesInfoValue
+type UpdatesInfoValue struct {
+	ChangeStatus            *string   `json:"ChangeStatus,omitempty"`
+	CompID                  *string   `json:"CompId,omitempty"`
+	Date                    *DateTime `json:"Date,omitempty"`
+	FileName                *string   `json:"FileName,omitempty"`
+	Index                   *string   `json:"Index,omitempty"`
+	KlupdsrvBundleDwlDate   *DateTime `json:"KLUPDSRV_BUNDLE_DWL_DATE,omitempty"`
+	KlupdsrvBundleID        *string   `json:"KLUPDSRV_BUNDLE_ID,omitempty"`
+	KlupdsrvBundleTypeDescr *string   `json:"KLUPDSRV_BUNDLE_TYPE_DESCR,omitempty"`
+	LocalPath               *string   `json:"LocalPath,omitempty"`
+	RelativeSrvPath         *string   `json:"RelativeSrvPath,omitempty"`
+	Type                    *string   `json:"Type,omitempty"`
+	Size                    *Size     `json:"Size,omitempty"`
+	Stt                     *string   `json:"Stt,omitempty"`
+}
+
+// GetUpdatesInfo lists of retransmissions.
+func (upd *Updates) GetUpdatesInfo(ctx context.Context, params UpdatesInfoParams) (*UpdatesInfos, error) {
+	postData, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest("POST", upd.client.Server+"/api/v1.0/Updates.GetUpdatesInfo", bytes.NewBuffer(postData))
 
 	if err != nil {
 		return nil, err
 	}
-	raw, err := upd.client.Do(ctx, request, nil)
-	return raw, err
+
+	updatesInfos := new(UpdatesInfos)
+	_, err = upd.client.Do(ctx, request, &updatesInfos)
+	return updatesInfos, err
 }
 
-//	Asynchronously remove updates.
-//
-//	Returns:
-//	- (string) request id of asynchronous operation, to cancel call Updates.RemoveUpdatesCancel,
-//	to get status call AsyncActionStateChecker.CheckActionState with returned request id as action guid
-func (upd *Updates) RemoveUpdates(ctx context.Context) (*RequestID, []byte, error) {
-	request, err := http.NewRequest("POST", upd.client.Server+"/api/v1.0/Updates.RemoveUpdates",
-		nil)
+// RemoveUpdates Asynchronously remove updates.
+func (upd *Updates) RemoveUpdates(ctx context.Context) (*PxgValStr, error) {
+	request, err := http.NewRequest("POST", upd.client.Server+"/api/v1.0/Updates.RemoveUpdates", nil)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	requestID := new(RequestID)
-	raw, err := upd.client.Do(ctx, request, &requestID)
-	return requestID, raw, err
+	pxgValStr := new(PxgValStr)
+	_, err = upd.client.Do(ctx, request, &pxgValStr)
+	return pxgValStr, err
 }
 
-//	Cancel asynchronous operation RemoveUpdates.
-//
-//	Parameters:
-//	- strRequestId	(string) request id of asynchronous operation Updates.RemoveUpdates
-func (upd *Updates) RemoveUpdatesCancel(ctx context.Context, strRequestId string) (*RequestID, []byte, error) {
+// RemoveUpdatesCancel Cancel asynchronous operation RemoveUpdates.
+func (upd *Updates) RemoveUpdatesCancel(ctx context.Context, strRequestId string) error {
 	postData := []byte(fmt.Sprintf(`{"strRequestId": "%s"}`, strRequestId))
-	request, err := http.NewRequest("POST", upd.client.Server+"/api/v1.0/Updates.RemoveUpdatesCancel",
-		bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", upd.client.Server+"/api/v1.0/Updates.RemoveUpdatesCancel", bytes.NewBuffer(postData))
 	if err != nil {
-		return nil, nil, err
+		return err
 	}
 
-	requestID := new(RequestID)
-	raw, err := upd.client.Do(ctx, request, &requestID)
-	return requestID, raw, err
+	_, err = upd.client.Do(ctx, request, nil)
+	return err
 }
