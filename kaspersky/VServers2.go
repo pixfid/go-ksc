a91678c9324aa8a22602c9892f987f9b9b4b6401
pixfid <pixfid@gmail.com>
2020-06-24 12:58:37 +0300

    refactor\cleanup VServers2
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/VServers2.go
+++ kaspersky/VServers2.go
@@ -31,11 +31,7 @@ import (
 	"net/http"
 )
 
-// VServers2 Class Reference
-//
-// Virtual servers processing..
-//
-// List of all members:
+// VServers2 Virtual servers processing..
 type VServers2 service
 
 // VServerStatistic struct
@@ -45,40 +41,66 @@ type VServerStatistic struct {
 
 // VSStatistic struct
 type VSStatistic struct {
-	KlvsrvCreated   *Klvsrv       `json:"KLVSRV_CREATED,omitempty"`
-	KlvsrvGroups    int64         `json:"KLVSRV_GROUPS,omitempty"`
-	KlvsrvHosts     int64         `json:"KLVSRV_HOSTS,omitempty"`
-	KlvsrvLicenses  []interface{} `json:"KLVSRV_LICENSES"`
-	KlvsrvMdmios    int64         `json:"KLVSRV_MDMIOS,omitempty"`
-	KlvsrvMobilies  int64         `json:"KLVSRV_MOBILIES,omitempty"`
-	KlvsrvProducts  *Klvsrv       `json:"KLVSRV_PRODUCTS,omitempty"`
-	KlvsrvProducts2 *Klvsrv       `json:"KLVSRV_PRODUCTS_2,omitempty"`
-	KlvsrvUsers     int64         `json:"KLVSRV_USERS,omitempty"`
+	KlvsrvCreated   *DateTime       `json:"KLVSRV_CREATED,omitempty"`
+	KlvsrvGroups    *int64          `json:"KLVSRV_GROUPS,omitempty"`
+	KlvsrvHosts     *int64          `json:"KLVSRV_HOSTS,omitempty"`
+	KlvsrvLicenses  []KlvsrvLicense `json:"KLVSRV_LICENSES,omitempty"`
+	KlvsrvMdmios    *int64          `json:"KLVSRV_MDMIOS,omitempty"`
+	KlvsrvMobilies  *int64          `json:"KLVSRV_MOBILIES,omitempty"`
+	KlvsrvProducts  *KlvsrvProducts `json:"KLVSRV_PRODUCTS,omitempty"`
+	KlvsrvProducts2 *KlvsrvProducts `json:"KLVSRV_PRODUCTS_2,omitempty"`
+	KlvsrvUsers     *int64          `json:"KLVSRV_USERS,omitempty"`
+}
+
+type KlvsrvLicense struct {
+	Type               *string             `json:"type,omitempty"`
+	KlvsrvLicenseValue *KlvsrvLicenseValue `json:"value,omitempty"`
+}
+
+type KlvsrvLicenseValue struct {
+	KllicKeyType   *int64    `json:"KLLIC_KEY_TYPE,omitempty"`
+	KllicLimitDate *DateTime `json:"KLLIC_LIMIT_DATE,omitempty"`
+	KllicProdName  *string   `json:"KLLIC_PROD_NAME,omitempty"`
+	KllicSerial    *string   `json:"KLLIC_SERIAL,omitempty"`
 }
 
 // Klvsrv struct
-type Klvsrv struct {
-	Type  string `json:"type,omitempty"`
-	Value string `json:"value"`
+type KlvsrvProducts struct {
+	Type  *string                 `json:"type,omitempty"`
+	Value *map[string]KlvsrvProds `json:"value,omitempty"`
 }
 
-// GetVServerStatistic
-// Acquire info on virtual server.
-// Returns info about the specified virtual server
-//
-// Parameters:
-//	- lVsId	(int64) virtual server id
-//
-// Returns:
-//	- (params) a container, see Virtual server statistic.
-func (vs *VServers2) GetVServerStatistic(ctx context.Context, lVsId int) (*VServerStatistic, []byte, error) {
+type KlvsrvProds struct {
+	Type  *string                       `json:"type,omitempty"`
+	Value *map[string]KlvsrvProdVersion `json:"value,omitempty"`
+}
+
+type KlvsrvProdVersion struct {
+	Type            *string         `json:"type,omitempty"`
+	KlvsrvProdValue KlvsrvProdValue `json:"value,omitempty"`
+}
+
+type KlvsrvProdValue struct {
+	KlvsrvProdCnt    *int64             `json:"KLVSRV_PROD_CNT,omitempty"`
+	KlvsrvProdDN     *string            `json:"KLVSRV_PROD_DN,omitempty"`
+	KlvsrvProdDv     *string            `json:"KLVSRV_PROD_DV,omitempty"`
+	KlvsrvProdBuilds []KlvsrvProdBuilds `json:"KLVSRV_PROD_BUILDS,omitempty"`
+}
+
+type KlvsrvProdBuilds struct {
+	Type                 *string         `json:"type,omitempty"`
+	KlvsrvProdBuildValue KlvsrvProdValue `json:"value,omitempty"`
+}
+
+// GetVServerStatistic Acquire info on virtual server. Returns info about the specified virtual server
+func (vs *VServers2) GetVServerStatistic(ctx context.Context, lVsId int) (*VServerStatistic, error) {
 	postData := []byte(fmt.Sprintf(`{"lVsId": %d}`, lVsId))
 	request, err := http.NewRequest("POST", vs.client.Server+"/api/v1.0/VServers2.GetVServerStatistic", bytes.NewBuffer(postData))
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	vServerStatistic := new(VServerStatistic)
-	raw, err := vs.client.Do(ctx, request, &vServerStatistic)
-	return vServerStatistic, raw, err
+	_, err = vs.client.Do(ctx, request, &vServerStatistic)
+	return vServerStatistic, err
 }
