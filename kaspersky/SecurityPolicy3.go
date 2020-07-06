2ff6fae7bbfb5c88a3c17527f2c21ea4e6a98970
pixfid <pixfid@gmail.com>
2020-07-06 22:48:54 +0300

    improved and refactor SecurityPolicy3 service add missing params types for UpdateSecurityGroup, AddSecurityGroup methods, cleanup return params types
    Signed-off-by: pixfid <pixfid@gmail.com>

--- kaspersky/SecurityPolicy3.go
+++ kaspersky/SecurityPolicy3.go
@@ -40,33 +40,46 @@ import (
 // User Permissions functional area on Administration server.
 type SecurityPolicy3 service
 
-// AddSecurityGroup Creates a security group on a server.
+// SecurityGroupParams struct
+type SecurityGroupParams struct {
+	// LVsID id of a virtual server, a value = 0 means main server.
+	// It is ignored in case of connection to virtual server.
+	LVsID      int64      `json:"lVsId,omitempty"`
+	PGrpParams PGrpParams `json:"pGrpParams,omitempty"`
+}
 
+type PGrpParams struct {
+	Type  string    `json:"type,omitempty"`
+	Value PGrpValue `json:"value,omitempty"`
+}
+
+type PGrpValue struct {
+	// KlsplSECGrpName name of a group,
+	KlsplSECGrpName string `json:"KLSPL_SEC_GRP_NAME,omitempty"`
+
+	// KlsplSECGrpDesc description of a group.
+	KlsplSECGrpDesc string `json:"KLSPL_SEC_GRP_DESC,omitempty"`
+}
+
+// AddSecurityGroup Creates a security group on a server.
 // lVsId	(int64) id of a virtual server, a value = 0 means main server.
 //
 // It is ignored in case of connection to virtual server.
 //
-//	Structure Example:
-//	{
-//	  "lVsId" : 0,
-//	  "pGrpParams" : {
-//	    "type" : "params",
-//	    "value" : {
-//	      "KLSPL_SEC_GRP_NAME" : "SECURITY_GROUP_NAME",
-//	      "KLSPL_SEC_GRP_DESC" : "SECURITY_GROUP_DESCRIPTION"
-//	    }
-//	  }
-//	}
-func (sp *SecurityPolicy3) AddSecurityGroup(ctx context.Context, params interface{}) ([]byte, error) {
-	postData, _ := json.Marshal(params)
-	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.AddSecurityGroup",
-		bytes.NewBuffer(postData))
+func (sp *SecurityPolicy3) AddSecurityGroup(ctx context.Context, params SecurityGroupParams) (*PxgValInt, error) {
+	postData, err := json.Marshal(params)
 	if err != nil {
 		return nil, err
 	}
 
-	raw, err := sp.client.Do(ctx, request, nil)
-	return raw, err
+	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.AddSecurityGroup", bytes.NewBuffer(postData))
+	if err != nil {
+		return nil, err
+	}
+
+	pxgValInt := new(PxgValInt)
+	_, err = sp.client.Do(ctx, request, &pxgValInt)
+	return pxgValInt, err
 }
 
 // AddUserIntoSecurityGroup Adds user into a security group.
@@ -78,97 +91,91 @@ func (sp *SecurityPolicy3) AddSecurityGroup(ctx context.Context, params interfac
 // A user located on a virtual server can be added only into a group located on this virtual server, otherwise a error occurs.
 //
 // A user located on a main server can be added only into a group located on this main server, otherwise a error occurs.
-func (sp *SecurityPolicy3) AddUserIntoSecurityGroup(ctx context.Context, lUserId, lGrpId int64) ([]byte, error) {
+func (sp *SecurityPolicy3) AddUserIntoSecurityGroup(ctx context.Context, lUserId, lGrpId int64) error {
 	postData := []byte(fmt.Sprintf(`{"lUserId": %d, "lGrpId": %d}`, lUserId, lGrpId))
-	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.AddUserIntoSecurityGroup",
-		bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.AddUserIntoSecurityGroup", bytes.NewBuffer(postData))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	raw, err := sp.client.Do(ctx, request, nil)
-	return raw, err
+	_, err = sp.client.Do(ctx, request, nil)
+	return err
 }
 
 // CloseUserConnections Closes user connections.
-func (sp *SecurityPolicy3) CloseUserConnections(ctx context.Context, lUserId int64) ([]byte, error) {
+func (sp *SecurityPolicy3) CloseUserConnections(ctx context.Context, lUserId int64) error {
 	postData := []byte(fmt.Sprintf(`{"lUserId": %d }`, lUserId))
-	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.CloseUserConnections",
-		bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.CloseUserConnections", bytes.NewBuffer(postData))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	raw, err := sp.client.Do(ctx, request, nil)
-	return raw, err
+	_, err = sp.client.Do(ctx, request, nil)
+	return err
 }
 
 // DeleteSecurityGroup Delete a security group.
 //
 // Connection to a virtual server has access only to groups which located on this virtual server.
-func (sp *SecurityPolicy3) DeleteSecurityGroup(ctx context.Context, lGrpId int64) ([]byte, error) {
+func (sp *SecurityPolicy3) DeleteSecurityGroup(ctx context.Context, lGrpId int64) error {
 	postData := []byte(fmt.Sprintf(`{"lGrpId": %d}`, lGrpId))
-	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.DeleteSecurityGroup",
-		bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.DeleteSecurityGroup", bytes.NewBuffer(postData))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	raw, err := sp.client.Do(ctx, request, nil)
-	return raw, err
+	_, err = sp.client.Do(ctx, request, nil)
+	return err
 }
 
 // DeleteUserFromSecurityGroup Removes user from a security group.
 //
 // Connection to a virtual server has access only to groups and users which located on this virtual server.
-func (sp *SecurityPolicy3) DeleteUserFromSecurityGroup(ctx context.Context, lUserId, lGrpId int64) ([]byte, error) {
+func (sp *SecurityPolicy3) DeleteUserFromSecurityGroup(ctx context.Context, lUserId, lGrpId int64) error {
 	postData := []byte(fmt.Sprintf(`{"lUserId": %d, "lGrpId": %d}`, lUserId, lGrpId))
-	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.DeleteUserFromSecurityGroup",
-		bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.DeleteUserFromSecurityGroup", bytes.NewBuffer(postData))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	raw, err := sp.client.Do(ctx, request, nil)
-	return raw, err
+	_, err = sp.client.Do(ctx, request, nil)
+	return err
 }
 
 // MoveUserIntoOtherSecurityGroup Moves user from one security group into other security group.
-func (sp *SecurityPolicy3) MoveUserIntoOtherSecurityGroup(ctx context.Context, lUserId, lGrpIdFrom, lGrpIdTo int64) ([]byte,
-	error) {
+func (sp *SecurityPolicy3) MoveUserIntoOtherSecurityGroup(ctx context.Context, lUserId, lGrpIdFrom, lGrpIdTo int64) error {
 	postData := []byte(fmt.Sprintf(`{"lUserId": %d, "lGrpIdFrom": %d, "lGrpIdTo": %d}`, lUserId, lGrpIdFrom, lGrpIdTo))
-	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.MoveUserIntoOtherSecurityGroup",
-		bytes.NewBuffer(postData))
+	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.MoveUserIntoOtherSecurityGroup", bytes.NewBuffer(postData))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	raw, err := sp.client.Do(ctx, request, nil)
-	return raw, err
+	_, err = sp.client.Do(ctx, request, nil)
+	return err
+}
+
+// SecurityGroupParams struct
+type UpdateSecurityGroupParams struct {
+	// lGrpId id of a created group.
+	LGrpId int64 `json:"lGrpId,omitempty"`
+	// PGrpParams parameters of a group.
+	PGrpParams PGrpParams `json:"pGrpParams,omitempty"`
 }
 
 // UpdateSecurityGroup Security group update.
-//
-//	Structure Example:
-//	{
-//	  "lVsId" : 0,
-//	  "pGrpParams" : {
-//	    "type" : "params",
-//	    "value" : {
-//	      "KLSPL_SEC_GRP_NAME" : "SECURITY_GROUP_NAME",
-//	      "KLSPL_SEC_GRP_DESC" : "SECURITY_GROUP_DESCRIPTION"
-//	    }
-//	  }
-//	}
-func (sp *SecurityPolicy3) UpdateSecurityGroup(ctx context.Context, params interface{}) ([]byte,
-	error) {
-	postData, _ := json.Marshal(params)
-	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.UpdateSecurityGroup",
-		bytes.NewBuffer(postData))
+// If a group does not exist a error occurs.
+// Connection to a virtual server has access only to groups which located on this virtual server.
+func (sp *SecurityPolicy3) UpdateSecurityGroup(ctx context.Context, params UpdateSecurityGroupParams) error {
+	postData, err := json.Marshal(params)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	raw, err := sp.client.Do(ctx, request, nil)
-	return raw, err
+	request, err := http.NewRequest("POST", sp.client.Server+"/api/v1.0/SecurityPolicy3.UpdateSecurityGroup", bytes.NewBuffer(postData))
+	if err != nil {
+		return err
+	}
+
+	_, err = sp.client.Do(ctx, request, nil)
+	return err
 }
